2183307e3c013d8cb6827687506e928e758f9a3e
boxdox <19517445+boxdox@users.noreply.github.com>
2022-09-23 18:37:27 +0530

    added: handle bot updates

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,37 @@
 import { Bot } from 'grammy'
 
+import { generateLink, parseText } from './parser'
+import { HELP_TEXT, replies } from './constants'
+
 export const createBot = (token: string) => {
   const bot = new Bot(token)
+
+  /*
+    handle help command
+  */
+  bot.command('help', ctx => {
+    ctx.reply(HELP_TEXT)
+  })
+
+  /*
+    handle message
+  */
+  bot.on('message', ctx => {
+    const message = ctx.msg.text
+
+    if (!message) {
+      return ctx.reply(replies.EMPTY_MESSAGE)
+    }
+    const parsedText = parseText(message)
+
+    if (parsedText.phone.length === 0) {
+      return ctx.reply(replies.NO_PHONE_NUMBER)
+    }
+
+    const link = generateLink(parsedText)
+
+    return ctx.reply(link)
+  })
 
   return bot
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,13 @@
 export const WHITESPACE_RE = /\s+/g
 export const NUMBER_RE = /[0-9+]+/
 
-export const WA_BASE_API = 'https://api.whatsapp.com/send'
+export const WA_BASE_API = `https://api.whatsapp.com/send`
+
+export const HELP_TEXT = `i am bot that convert phone number (with optional message) into a link that allows you to chat with someone on whatsapp without saving their contact
+send me a message with phone number and optionally a message. note: we don't store anything you have entered. source for this bot is available on github
+example message: "+9999 999 999 hi there", will return: https://api.whatsapp.com/send?phone=9999999999&text=hi+there
+`
+export const replies = {
+  EMPTY_MESSAGE: `can't process empty messages :(`,
+  NO_PHONE_NUMBER: `did you forget to pass a phone number?`,
+} as const
